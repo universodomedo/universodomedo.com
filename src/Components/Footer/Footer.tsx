788e529e68d701981be8a44c0e5821d47b5cd6af
caio.vigiani <caio.vigiani@hotmail.com>
2024-07-13 23:03:07 -0300

    - Social Media no Footer - Instalado FontAwesome

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,16 +1,20 @@
 import "./Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiscord, faSpotify, faYoutube, faTwitch } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-end'>
         <div className="curved-corners left"></div>
-        <ul className="social-icons">
-          <li><a href="#" className="fab fa-facebook-f"></a></li>
-          <li><a href="#" className="fab fa-twitter"></a></li>
-          <li><a href="#" className="fab fa-linkedin-in"></a></li>
-          <li><a href="#" className="fab fa-instagram"></a></li>
-        </ul>
+        <div className="social-media">
+          <ul className="social-icons">
+            <li><a target="_blank" href="https://discord.universodomedo.com"><FontAwesomeIcon icon={faDiscord} /></a></li>
+            <li><a target="_blank" href="https://open.spotify.com/show/10qzPjLpugVhzn90ufDBuN"><FontAwesomeIcon icon={faSpotify} /></a></li>
+            <li><a target="_blank" href="https://youtube.universodomedo.com"><FontAwesomeIcon icon={faYoutube} /></a></li>
+            <li><a target="_blank" href="https://twitch.universodomedo.com"><FontAwesomeIcon icon={faTwitch} /></a></li>
+          </ul>
+        </div>
         <div className="curved-corners right"></div>
       </div>
     </div>
