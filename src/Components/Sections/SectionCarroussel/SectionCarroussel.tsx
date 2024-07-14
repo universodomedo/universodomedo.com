5e3d16dfd9573d85b25273b9a8b6b0ebe48ada65
caio.vigiani <caio.vigiani@hotmail.com>
2024-07-13 21:33:27 -0300

    Implementação do React Slick (Carousel)

--- src/Components/Sections/SectionCarroussel/SectionCarroussel.tsx
+++ src/Components/Sections/SectionCarroussel/SectionCarroussel.tsx
@@ -2,11 +2,16 @@ import "./SectionCarroussel.css";
 import CarrosselItem from '../../SubComponents/CarrosselItem/CarrosselItem';
 import { useApi } from "../../../ApiConsumer/Consumer.tsx";
 import { useEffect, useState } from "react";
+import Slider from "react-slick";
 import { Campaign, CampaignSession, CampaignSessionWithRelations } from "udm-types";
 
 import imgCapa1 from "../../Assets/testeCapa1.png";
 import imgCapa2 from "../../Assets/testeCapa2.png";
 import imgCapa3 from "../../Assets/testeCapa3.png";
+
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SectionCarroussel = () => {
   const [fetchedData, setFetchedData] = useState<CampaignSessionWithRelations[]>([{} as CampaignSessionWithRelations]);
@@ -20,13 +25,26 @@ const SectionCarroussel = () => {
     fetchData();
   }, []);
 
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  }
+
   return (
-    <div className='section-content'>
+    <div className='carousel-content'>
       <div className='title'>
         <p>Próximas Sessões</p>
       </div>
 
-      
+      <div className="slider-container">
+        <Slider {...settings}>
+          <CarrosselItem itemTitle={"Apenas uma Prece"} imgUrl={imgCapa1} tsStart={new Date()} live={true}/>
+          <CarrosselItem itemTitle={"Black Ops"} imgUrl={imgCapa2} tsStart={new Date()} live={false}/>
+          <CarrosselItem itemTitle={"Antes do Abismo"} imgUrl={imgCapa3} tsStart={new Date()} live={false}/>
+        </Slider>
+      </div>
     </div>
   )
 }
