0843e52bb07bbc109d874059d6e0216c179f3133
caio.vigiani <caio.vigiani@hotmail.com>
2024-09-26 16:43:06 -0300

    Parcial

--- src/Components/ConsultaFicha/page.tsx
+++ src/Components/ConsultaFicha/page.tsx
@@ -1,7 +1,7 @@
 // #region Imports
 import { createContext, useContext, useState, useEffect, useRef } from "react";
 import style from "./style.module.css";
-import Select from "react-select";
+import Select, { ActionMeta, MultiValue } from "react-select";
 import { FiltroProps, OpcaoFormatada, CategoriaFormatada, FiltroPropsItems } from "Types/classes.tsx";
 import ValoresFiltrosSelecionados from "Components/ValoresFiltrosSelecionados/page.tsx";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,14 +9,18 @@ import { RootState } from 'Redux/store.ts';
 import { setCacheFiltros } from "Redux/slices/abasHelperSlice.ts";
 // #endregion
 
+interface OpcoesSelecionadas { idFiltro: number, idOpcao: string[] }
+
 interface ConsultaContextProps<T> {
     registros: T[];
     registrosFiltrados: T[];
     filtroProps: FiltroProps<T>;
-    ordenacao: { key: string | number | symbol | ((item: T) => any); direction: 'asc' | 'desc' } | null;
-    valoresFiltrosSelecionados: { [key: string]: string[] };
-    handleFiltro: (key: string | number | symbol | ((item: any) => any), values: string[]) => void;
-    handleOrdenacao: (key: string | number | symbol | ((item: T) => any), direction: 'asc' | 'desc') => void;
+    // ordenacao: { idFiltro: number, direction: 'asc' | 'desc' } | null;
+    valoresFiltrosSelecionados: OpcoesSelecionadas[];
+    handleFiltro: (opcoesSelecionadas: OpcoesSelecionadas[]) => void;
+    removeFiltro: (idFiltro: number, idOpcao: string) => void;
+    // handleOrdenacao: (key: string | number | symbol | ((item: T) => any), direction: 'asc' | 'desc') => void;
+    // setValoresFiltrosSelecionados: 
 };
 
 const ConsultaContext = createContext<ConsultaContextProps<any> | undefined>(undefined);
@@ -31,11 +35,10 @@ export const useConsultaContext = <T,>(): ConsultaContextProps<T> => {
     return context;
 };
 
-export const ConsultaProvider = <T,>({ abaId, children, registros, filtroProps, onLoadComplete }: { abaId:string; children: React.ReactNode; registros: T[]; filtroProps: FiltroProps<T>; onLoadComplete: () => void; }) => {
+export const ConsultaProvider = <T,>({ abaId, children, registros, filtroProps, onLoadComplete }: { abaId: string; children: React.ReactNode; registros: T[]; filtroProps: FiltroProps<T>; onLoadComplete: () => void; }) => {
     const [registrosFiltrados, setRegistrosFiltrados] = useState<T[]>(registros);
-    const [ordenacao, setOrdenacao] = useState<{ key: string | number | symbol | ((item: T) => any); direction: 'asc' | 'desc' } | null>(null);
-    const [valoresFiltrosSelecionados, setValoresFiltrosSelecionados] = useState<{ [key: string]: string[] }>({});
-    const [opcoesSelecionadas, setOpcoesSelecionadas] = useState<{ label: string; idFiltro: string; idOpcao: string }[]>([]);
+    const [ordenacao, setOrdenacao] = useState<{ idFiltro: number, direction: 'asc' | 'desc' } | null>(null);
+    const [valoresFiltrosSelecionados, setValoresFiltrosSelecionados] = useState<OpcoesSelecionadas[]>([]);
 
     const dispatch = useDispatch();
     const abaState = useSelector((state: RootState) => state.abasHelper[abaId]);
@@ -46,78 +49,86 @@ export const ConsultaProvider = <T,>({ abaId, children, registros, filtroProps, 
         return typeof key === 'function' ? key(item) : item[key];
     };
 
-    const handleFiltro = (key: string | number | symbol | ((item: any) => any), values: string[]) => {
-        setValoresFiltrosSelecionados((prev) => ({ ...prev, [key as string]: values }));
-    };
-
-    const handleOrdenacao = (key: string | number | symbol | ((item: T) => any), direction: 'asc' | 'desc') => {
-        setOrdenacao({ key, direction });
-
-        const sortedData = [...registrosFiltrados].sort((a, b) => {
-            const aValue = typeof key === "function" ? key(a) : (a[key as keyof T] as any);
-            const bValue = typeof key === "function" ? key(b) : (b[key as keyof T] as any);
-
-            if (aValue < bValue) return direction === "asc" ? -1 : 1;
-            if (aValue > bValue) return direction === "asc" ? 1 : -1;
-            
-            return 0;
+    const handleFiltro = (opcoesSelecionadas: OpcoesSelecionadas[]) => {
+        setValoresFiltrosSelecionados((prevState) => {
+            const filtrosSemOAtualizado = prevState.filter(
+                (filtro) => filtro.idFiltro !== opcoesSelecionadas[0].idFiltro
+            );
+    
+            return [...filtrosSemOAtualizado, ...opcoesSelecionadas];
         });
-
-        setRegistrosFiltrados(sortedData);
     };
 
-    const atualizarOpcoesSelecionadas = () => {
-        const opcoesAtualizadas = Object.entries(valoresFiltrosSelecionados).flatMap(([key, valoresSelecionados]) => {
-            const filtroConfig = filtroProps.items.find(f => f.key === key);
-            if (!filtroConfig || !filtroConfig.options) {
-                return [];
-            }
-
-            return valoresSelecionados.map(valorSelecionado => {
-                const opcao = filtroConfig.temCategorias()
-                    ? (filtroConfig.options as CategoriaFormatada[]).flatMap(categoria => categoria.options).find(opcao => opcao.value === valorSelecionado)
-                    : (filtroConfig.options as OpcaoFormatada[]).find(opcao => opcao.value === valorSelecionado);
-
-                return {
-                    label: opcao?.label || valorSelecionado,
-                    idFiltro: key,
-                    idOpcao: valorSelecionado
-                };
-            });
+    const removeFiltro = (idFiltro: number, idOpcao: string) => {
+        setValoresFiltrosSelecionados((prevValores) => {
+            // Copia o estado anterior
+            const novoEstado = prevValores.map((filtro) => {
+                // Verifica se o filtro atual é o que queremos modificar
+                if (filtro.idFiltro === idFiltro) {
+                    // Remove a opção específica do idOpcao
+                    const novasOpcoes = filtro.idOpcao.filter((opcao) => opcao !== idOpcao);
+                    
+                    // Retorna o filtro atualizado, ou seja, com a nova lista de opções
+                    return { ...filtro, idOpcao: novasOpcoes };
+                }
+                return filtro;
+            }).filter(filtro => filtro.idOpcao.length > 0); // Remove filtros sem opções
+    
+            return novoEstado;
         });
-
-        setOpcoesSelecionadas(opcoesAtualizadas);
     };
 
-    useEffect(() => {
-        if (abaState && abaState.filtros) {
-            setValoresFiltrosSelecionados(abaState.filtros);
-        }
+    // const handleOrdenacao = (key: string | number | symbol | ((item: T) => any), direction: 'asc' | 'desc') => {
+    //     setOrdenacao({ key, direction });
 
-        onLoadComplete();
-    }, []);
+    //     const sortedData = [...registrosFiltrados].sort((a, b) => {
+    //         const aValue = typeof key === "function" ? key(a) : (a[key as keyof T] as any);
+    //         const bValue = typeof key === "function" ? key(b) : (b[key as keyof T] as any);
+
+    //         if (aValue < bValue) return direction === "asc" ? -1 : 1;
+    //         if (aValue > bValue) return direction === "asc" ? 1 : -1;
+
+    //         return 0;
+    //     });
+
+    //     setRegistrosFiltrados(sortedData);
+    // };
+
+    // useEffect(() => {
+    //     if (abaState && abaState.filtros) {
+    //         setValoresFiltrosSelecionados(abaState.filtros);
+    //     }
+
+    //     onLoadComplete();
+    // }, []);
 
     useEffect(() => {
         filtrosRef.current = valoresFiltrosSelecionados;
 
         const dadosFiltrados = registros.filter((item) => {
-            return filtroProps.items.every((config) => {
-                const filtroValores = valoresFiltrosSelecionados[config.key as string] || [];
-                const itemValor = String(getKeyValue(item, config.key)).toLowerCase();
+            return valoresFiltrosSelecionados.every((filtro) => {
+                const idFiltro = filtro.idFiltro;
+                const opcoesSelecionadas = filtro.idOpcao;
 
-                return (filtroValores.length === 0 || filtroValores.some((filtro) => itemValor.includes(filtro.toLowerCase())));
+                const filtroConfig = filtroProps.items[idFiltro];
+
+                if (!filtroConfig) return true;
+
+                const itemValor = String(getKeyValue(item, filtroConfig.key)).toLowerCase();
+
+                if (filtroConfig.filterType === "select")
+                    return (opcoesSelecionadas.length === 0 || opcoesSelecionadas.some((opcaoId) => itemValor === opcaoId.toLowerCase()));
+                else 
+                    return (opcoesSelecionadas.length === 0 || opcoesSelecionadas.some((opcaoId) => itemValor.includes(opcaoId.toLowerCase())));
             });
         });
 
         setRegistrosFiltrados(dadosFiltrados);
-
-        return () => {
-            dispatch(setCacheFiltros({ abaId, filtros: valoresFiltrosSelecionados }));
-        };
     }, [valoresFiltrosSelecionados]);
 
     return (
-        <ConsultaContext.Provider value={{ registros, registrosFiltrados, filtroProps, ordenacao, valoresFiltrosSelecionados, handleFiltro, handleOrdenacao }}>
+        <ConsultaContext.Provider value={{ registros, registrosFiltrados, filtroProps, valoresFiltrosSelecionados, handleFiltro, removeFiltro }}>
+            {/* <ConsultaContext.Provider value={{ registros, registrosFiltrados, filtroProps, ordenacao, valoresFiltrosSelecionados, handleFiltro, handleOrdenacao }}> */}
             {children}
         </ConsultaContext.Provider>
     );
@@ -151,39 +162,58 @@ const Filtro = <T,>() => {
     return (
         <div className={style.div_filtros}>
             {filtroProps.items.map((config, index) => (
-                <FiltroItem key={index} config={config as FiltroPropsItems<T>} />
+                <FiltroItem key={index} idFiltro={index} config={config as FiltroPropsItems<T>} />
             ))}
         </div>
     );
 };
 
-const FiltroItem = <T,>({ config } : { config: FiltroPropsItems<T> }) => {
-    const { ordenacao, handleOrdenacao } = useConsultaContext<T>();
+const FiltroItem = <T,>({ idFiltro, config }: { idFiltro: number, config: FiltroPropsItems<T> }) => {
+    // const { ordenacao, handleOrdenacao } = useConsultaContext<T>();
 
-    const handleClickOrdenacao = (key: keyof T | ((item: T) => any)) => {
-        if (ordenacao?.key === key) {
-            handleOrdenacao(key, ordenacao.direction === 'asc' ? 'desc' : 'asc');
-        } else {
-            handleOrdenacao(key, 'asc');
-        }
-    };
+    // const handleClickOrdenacao = (key: keyof T | ((item: T) => any)) => {
+    //     if (ordenacao?.key === key) {
+    //         handleOrdenacao(key, ordenacao.direction === 'asc' ? 'desc' : 'asc');
+    //     } else {
+    //         handleOrdenacao(key, 'asc');
+    //     }
+    // };
 
     return (
         <div className={style.div_filtro}>
-            <span onClick={() => config.sortEnabled && handleClickOrdenacao(config.key)}>
+            {/* <span onClick={() => config.sortEnabled && handleClickOrdenacao(config.key)}> */}
+            <span>
                 {config.label}
-                {ordenacao?.key === config.key && (
+                {/* {ordenacao?.key === config.key && (
                     <span>{ordenacao!.direction === 'asc' ? ' ↑' : ' ↓'}</span>
-                )}
+                )} */}
             </span>
 
-            <CaixaFiltroItem config={config} />
+            <CaixaFiltroItem idFiltro={idFiltro} config={config} />
         </div>
     );
 }
 
-const CaixaFiltroItem = <T,>({ config } : { config: FiltroPropsItems<T> }) => {
-    const { valoresFiltrosSelecionados, handleFiltro } = useConsultaContext<T>();
+const CaixaFiltroItem = <T,>({ idFiltro, config }: { idFiltro: number, config: FiltroPropsItems<T> }) => {
+    const { filtroProps, valoresFiltrosSelecionados, handleFiltro } = useConsultaContext<T>();
+
+    const filtroAtual = valoresFiltrosSelecionados.find((filtro) => filtro.idFiltro === idFiltro);
+
+    const opcoesSelecionadas = filtroAtual ? filtroAtual.idOpcao : [];
+
+    const handleChangeMulti = (selectedOptions: MultiValue<{ value: string; label: string }>, actionMeta: ActionMeta<{ value: string; label: string }>) => {
+        const opcoesMapeadas = selectedOptions.map((option) => option.value);
+        
+        const objetoFiltro = { idFiltro, idOpcao: opcoesMapeadas };
+
+        handleFiltro([objetoFiltro]);
+    };
+
+    const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const currentText = event.target.value;
+
+        handleFiltro([{idFiltro, idOpcao: [currentText]}]);
+    }
 
     return (
         <>
@@ -193,21 +223,41 @@ const CaixaFiltroItem = <T,>({ config } : { config: FiltroPropsItems<T> }) => {
                     value={
                         config.temCategorias()
                             ? (config.options as CategoriaFormatada[])
-                                .flatMap(categoria => categoria.options)
-                                .filter(option => valoresFiltrosSelecionados[config.key as string]?.includes(option.value))
-                                .map(option => ({ value: option.value, label: option.label }))
+                                .flatMap((categoria) => categoria.options)
+                                .filter((option) => opcoesSelecionadas.includes(String(option.value)))
+                                .map((option) => ({ value: option.value, label: option.label }))
                             : (config.options as OpcaoFormatada[])
-                                .filter((option) => valoresFiltrosSelecionados[config.key as string]?.includes(option.value))
+                                .filter((option) => opcoesSelecionadas.includes(String(option.value)))
                                 .map((option) => ({ value: option.value, label: option.label }))
                     }
-                    options={config.options}
-                    onChange={(selectedOptions) => handleFiltro(config.key, selectedOptions.map((option) => option.value))}
+                    options={
+                        config.temCategorias()
+                            ? (config.options as CategoriaFormatada[]).flatMap((categoria) => categoria.options)
+                            : (config.options as OpcaoFormatada[])
+                    }
+                    // onChange={(selectedOptions) =>
+                    //     handleFiltro([{ idFiltro, idOpcao: selectedOptions.map((option) => String(option.value)) }])
+                    // }
+                    onChange={handleChangeMulti}
                     placeholder={`Selecione`}
                     styles={{
-                        control: (provided) => ({ ...provided, flexGrow: 1, minWidth: 0, maxWidth: '100%', overflow: 'hidden', whiteSpace: 'nowrap' }),
+                        control: (provided) => ({
+                            ...provided,
+                            flexGrow: 1,
+                            minWidth: 0,
+                            maxWidth: '100%',
+                            overflow: 'hidden',
+                            whiteSpace: 'nowrap',
+                        }),
                         multiValue: (provided) => ({ ...provided, maxWidth: '100%', overflow: 'hidden', textOverflow: 'ellipsis' }),
-                        valueContainer: (provided) => ({ ...provided, display: 'flex', flexWrap: 'nowrap', overflow: 'hidden', whiteSpace: 'nowrap' }),
-                        multiValueLabel: (provided) => ({ ...provided, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' })
+                        valueContainer: (provided) => ({
+                            ...provided,
+                            display: 'flex',
+                            flexWrap: 'nowrap',
+                            overflow: 'hidden',
+                            whiteSpace: 'nowrap',
+                        }),
+                        multiValueLabel: (provided) => ({ ...provided, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }),
                     }}
                     controlShouldRenderValue={false}
                 />
@@ -215,10 +265,10 @@ const CaixaFiltroItem = <T,>({ config } : { config: FiltroPropsItems<T> }) => {
                 <input
                     type={config.filterType}
                     placeholder={`Filtrar por ${config.label}`}
-                    value={valoresFiltrosSelecionados[config.key as string]?.[0] || ""}
-                    onChange={(e) => handleFiltro(config.key, [e.target.value])}
+                    value={opcoesSelecionadas[0] || ""}
+                    onChange={handleChangeText}
                 />
             )}
         </>
     );
-}
+};
--- src/Components/ValoresFiltrosSelecionados/page.tsx
+++ src/Components/ValoresFiltrosSelecionados/page.tsx
@@ -5,58 +5,52 @@ import { OpcaoFormatada, CategoriaFormatada } from "Types/classes.tsx";
 // #endregion
 
 const page = <T,>() => {
-    const { valoresFiltrosSelecionados, filtroProps, handleFiltro } = useConsultaContext<T>();
+    const { filtroProps, valoresFiltrosSelecionados, removeFiltro } = useConsultaContext<T>();
 
-    const handleRemoveValue = (key: string, valorSelecionado: string) => {
-        const valoresAtualizados = valoresFiltrosSelecionados[key].filter(valor => valor !== valorSelecionado);
-        handleFiltro(key, valoresAtualizados);
+    const handleRemoveValue = (idFiltro: number, idOpcao: string) => {
+        removeFiltro(idFiltro, idOpcao);
     };
 
-    // Função para buscar o label pelo valor selecionado
-    const getLabelByValue = (key: string, valorSelecionado: string) => {
-        const filtroConfig = filtroProps.items.find(f => f.key === key);
-        
-        if (!filtroConfig || !filtroConfig.options) {
-            return valorSelecionado; // Retorna o próprio valor se não houver label correspondente
-        }
+    const obtemOpcoesSelecionadas = (): { valorSelecionado: string, idFiltro: number, idOpcao: string }[] => {
+        return valoresFiltrosSelecionados.flatMap((valorSelecionado) => {
+            const opcoesFiltro = filtroProps.items.filter(op => op.temOpcoes)[valorSelecionado.idFiltro].options as OpcaoFormatada[];
 
-        // Procura o label correspondente ao valor
-        const opcaoEncontrada = filtroConfig.temCategorias()
-            ? (filtroConfig.options as CategoriaFormatada[]).flatMap(categoria => categoria.options)
-                  .find(opcao => opcao.value === valorSelecionado)
-            : (filtroConfig.options as OpcaoFormatada[]).find(opcao => opcao.value === valorSelecionado);
-
-        return opcaoEncontrada ? opcaoEncontrada.label : valorSelecionado;
+            if (!opcoesFiltro) return [];
+    
+            return valorSelecionado.idOpcao.flatMap((opcaoSelecionada) => {
+                const opcao = opcoesFiltro.find(op => op.value === opcaoSelecionada);
+                return opcao ? [{
+                    valorSelecionado: opcao.label || opcao.value,
+                    idFiltro: valorSelecionado.idFiltro,
+                    idOpcao: opcaoSelecionada
+                }] : [];
+            });
+        });
     };
 
     return (
         <div className={style.div_teste}>
-            {/* {valoresFiltrosSelecionados.map((valorSelecionado: {key: string}, index:number) => (
-                <ValorSelecionado key={index} valorSelecionado={valorSelecionado.key}/>
-            ))} */}
-            {Object.entries(valoresFiltrosSelecionados).map(([key, valoresSelecionados]) => (
-                valoresSelecionados.map(valorSelecionado => (
-                    <ValorSelecionado key={key} valorSelecionado={getLabelByValue(key, valorSelecionado)} handleRemoveValue={() => handleRemoveValue(key, valorSelecionado)}/>
-                ))
+            {obtemOpcoesSelecionadas().map(({ valorSelecionado, idFiltro, idOpcao }, index) => (
+                <ValorSelecionado key={index} valorSelecionado={valorSelecionado} idFiltro={idFiltro} idOpcao={idOpcao} handleRemoveValue={handleRemoveValue}/>
             ))}
         </div>
     );
-}
+};
 
-const ValorSelecionado = ({ valorSelecionado, handleRemoveValue }: { valorSelecionado:string, handleRemoveValue: () => void }) => {
+const ValorSelecionado = ({ valorSelecionado, idFiltro, idOpcao, handleRemoveValue }: { valorSelecionado: string, idFiltro: number, idOpcao: string, handleRemoveValue: (idFiltro: number, idOpcao: string) => void }) => {
     return (
         <div className={style.value_container}>
             <div className={style.value}>{valorSelecionado}</div>
-            <BotaoFechar onClick={handleRemoveValue}/>
+            <BotaoFechar onClick={() => handleRemoveValue(idFiltro, idOpcao)} />
         </div>
     )
-}
+};
 
-const BotaoFechar = ({ id, onClick }: { id?: string; onClick?: (e: React.MouseEvent<HTMLDivElement>) => void }) => {
+const BotaoFechar = ({ onClick }: {onClick?: (e: React.MouseEvent<HTMLDivElement>) => void }) => {
     const svg = "M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z";
 
     return (
-        <div className={style.button_x} id={id} onClick={onClick} ><svg width={14} height={14} viewBox="0 0 20 20"><path d={svg}></path></svg></div>
+        <div className={style.button_x} onClick={onClick} ><svg width={14} height={14} viewBox="0 0 20 20"><path d={svg}></path></svg></div>
     );
 }
 
