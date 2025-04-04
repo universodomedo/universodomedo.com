ee6bf8d2bc8b88f94fe965807ae3d1fcec32506a
caio.vigiani <caio.vigiani@hotmail.com>
2025-04-03 23:48:44 -0300

    comentando temporariamente o ws

--- src/app/(sem-cabecalho)/minha-pagina/page-dados.tsx
+++ src/app/(sem-cabecalho)/minha-pagina/page-dados.tsx
@@ -21,7 +21,7 @@ export default function MinhaDisponibilidadeComDados({ dadosMinhaPagina }: { dad
                 <SecaoPosts />
             </div>
 
-            <SecaoContatos />
+            {/* <SecaoContatos /> */}
         </div>
     );
 }
