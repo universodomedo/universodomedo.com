11294e2e145156c25ddabc24ee77f86c84e4b028
Alef Hamad <alefhamad@gmail.com>
2025-04-04 01:07:10 -0300

    Change how the fronend handles the online user list

--- src/componentes/ElementosPaginaUsuario/Contato/page.tsx
+++ src/componentes/ElementosPaginaUsuario/Contato/page.tsx
@@ -4,18 +4,16 @@ import styles from './styles.module.css';
 import { useEffect, useState } from 'react';
 import { getSocket } from 'Libs/socket.ts';
 
-import { UsuarioDto } from 'types-nora-api';
-
-import Image from "next/image";
+import { StatusSocketUsuario } from 'types-nora-api';
 import ElementoAvatar from 'Uteis/ImagemLoader/ElementoAvatar';
 
 export default function SecaoContatos() {
-    const [usuarios, setUsuarios] = useState<UsuarioDto[]>([]);
+    const [usuarios, setUsuarios] = useState<StatusSocketUsuario[]>([]);
 
     useEffect(() => {
         const socket = getSocket();
 
-        socket.on('updateUsers', (usuarios: UsuarioDto[]) => {
+        socket.on('updateUsers', (usuarios: StatusSocketUsuario[]) => {
             setUsuarios(usuarios);
         });
 
@@ -28,25 +26,35 @@ export default function SecaoContatos() {
         <div id={styles.portal_usuario_direita}>
             <div className={styles.secao_contatos}>
                 <div className={styles.recipiente_lista_contatos}>
-                    {usuarios.map(usuario => (
-                        <Contato key={usuario.id} usuario={usuario} />
-                    ))}
+                    {usuarios
+                        .filter(({ usuario }) => usuario !== undefined)
+                        .map(({ usuario, status }) => (
+                            <Contato key={usuario!.id} usuario={usuario!} status={status} />
+                        ))}
                 </div>
             </div>
         </div>
     );
-};
+}
 
-function Contato({ usuario }: { usuario: UsuarioDto }) {
+function Contato({
+    usuario,
+    status
+}: {
+    usuario: NonNullable<StatusSocketUsuario['usuario']>;
+    status: boolean;
+}) {
     return (
         <div className={styles.recipiente_contato}>
             <div className={styles.recipiente_imagem_contato}>
-                <ElementoAvatar src={usuario.customizacao.caminhoAvatar} />
+                <ElementoAvatar src={usuario.customizacao?.caminhoAvatar || 'hi/avatar/0036ea88-0abd-46ae-8770-fcf9e9b89c96.png'} />
             </div>
             <div className={styles.recipiente_informacoes_contato}>
                 <h2>{usuario.username}</h2>
-                <span>Em Sessão</span>
+                <span style={{ color: status ? 'green' : 'gray' }}>
+                    {status ? 'Em Sessão' : 'Offline'}
+                </span>
             </div>
         </div>
     );
-};
+}
