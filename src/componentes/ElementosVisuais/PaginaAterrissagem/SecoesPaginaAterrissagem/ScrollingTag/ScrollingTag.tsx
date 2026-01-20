8ca6d307e52f8dea414bb6bda2523e906be8ba92
mrlowis <luizfernandogomesvale@gmail.com>
2026-01-20 08:19:52 -0300

    Seção de Etiquetas "finalizada".

--- src/componentes/ElementosVisuais/PaginaAterrissagem/SecoesPaginaAterrissagem/ScrollingTag/ScrollingTag.tsx
+++ src/componentes/ElementosVisuais/PaginaAterrissagem/SecoesPaginaAterrissagem/ScrollingTag/ScrollingTag.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 // direcao: o sentido que o item vai seguir
 // urlImagemExterna: caminho da imagem que vai ser inserida na parte de fora do item
 // urlImagemInterna: caminho da imagem que vai ser inserida na parte de dentro do item
@@ -5,16 +7,36 @@
 
 type ScrollingTagProps = {
 	direcao: 'esquerda-direita' | 'direita-esquerda';
-	urlImagemExterna: string;
-	urlImagemInterna: string;
-	conteudoTexto: string;
+	urlImagem: string;
+	conteudoTexto: { titulo: string, texto: string };
+	classNameExterno?: string;
 };
 
 
-export default function ScrollingTag({ direcao, urlImagemExterna, urlImagemInterna, conteudoTexto}: ScrollingTagProps) {
+export default function ScrollingTag({ direcao, urlImagem, conteudoTexto, classNameExterno }: ScrollingTagProps) {
+
 	return (
-		<div className={styles.recipiente_individual_scrolling_tag}>
-			{/* to do */}
+		<div className={`${styles.recipiente_individual_scrolling_tag} ${direcao == 'esquerda-direita' ? styles.esquerda_direita : styles.direita_esquerda} ${classNameExterno ?? ''} `}>
+			<div className={styles.recipiente_porta_bmk}>
+				<img className={styles.detalhe_porta_bmk} src="/imagensFigma/detalhe-porta-bmk.png" alt="#" />
+				<img className={styles.recipiente_porta_marcador} src="/imagensFigma/porta_marcador.png" alt="#" />
+				<div className={styles.mascara_anexo}>
+					<div className={styles.filtro_bmk}></div>
+					<img className={styles.recipiente_imagem_anexo} src={urlImagem} alt="#" /></div>
+			</div>
+
+			<a href="#">
+				<div className={styles.recipiente_marca_pagina}>
+
+					<img className={styles.marca_pagina} src="/imagensFigma/marca-pagina2.png" alt="#" />
+					
+					<div className={styles.recipiente_textos}>
+						<h3 >{conteudoTexto.titulo}</h3>
+						<p >{conteudoTexto.texto}</p>
+					</div>
+
+				</div>
+			</a>
 		</div>
 	);
 };
--- src/componentes/ElementosVisuais/PaginaAterrissagem/SecoesPaginaAterrissagem/SecaoTags/SecaoTags.tsx
+++ src/componentes/ElementosVisuais/PaginaAterrissagem/SecoesPaginaAterrissagem/SecaoTags/SecaoTags.tsx
@@ -5,8 +5,9 @@ import ScrollingTag from '../ScrollingTag/ScrollingTag';
 export default function SecaoTags() {
 	return (
 		<div className={styles.recipiente_tags}>
-			<ScrollingTag />
-			<ScrollingTag />
+			<ScrollingTag classNameExterno={styles.direita_esquerda} direcao={'direita-esquerda'} urlImagem={'/imagensFigma/capa-assistir.png'} conteudoTexto={{titulo: 'Assistir Aventuras', texto: 'Acompanhe nossas aventuras já gravadas.'}} />
+
+			<ScrollingTag classNameExterno={styles.esquerda_direita} direcao={'esquerda-direita'} urlImagem={'/imagensFigma/comunidade-capa.png'} conteudoTexto={{titulo: 'Comunidade', texto: 'Forme alianças na batalha contra o paranormal através de nossa comunidade do Discord.'}} />
 		</div>
 	);
 };
