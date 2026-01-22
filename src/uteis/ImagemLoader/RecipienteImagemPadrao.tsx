f8f759a290b065a20760b5d766301321e1250d19
caio.vigiani <caio.vigiani@hotmail.com>
2026-01-21 23:19:33 -0300

    corrigindo validação de tipos para aceitar webp

---
+++ src/uteis/ImagemLoader/RecipienteImagemPadrao.tsx
@@ -0,0 +1,5 @@
+import carregaImagem from 'Uteis/ImagemLoader/ImagemLoader.ts';
+
+export default function RecipienteImagemPadrao({ src, className }: { src: string | undefined; className?: string }) {
+    return <img alt='' src={carregaImagem({ src })} className={className} />;
+};
