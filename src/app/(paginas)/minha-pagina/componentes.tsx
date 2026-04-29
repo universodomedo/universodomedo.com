7d41b09678ca5f8f76d139e773f8d963498f52aa
Luiz Fernando <luizfernandogomesvale@gmail.com>
2026-04-28 21:46:45 -0300

    Upload de Imagens

--- src/app/(paginas)/minha-pagina/componentes.tsx
+++ src/app/(paginas)/minha-pagina/componentes.tsx
@@ -25,7 +25,7 @@ function MinhaPagina_Slot() {
                 <SecaoPosts />
             </div>
 
-            <SecaoContatos />
+            {/* <SecaoContatos /> */}
         </div>
     );
 };
