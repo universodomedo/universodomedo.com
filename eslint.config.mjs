3584c6bea8be68cd80bc8f24822c1708c677d5ac
caio.vigiani <caio.vigiani@hotmail.com>
2026-05-25 08:45:35 -0300

    Alteração para evitar new line no fim de arquivo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        rules: {
+            "eol-last": ["error", "never"],
+        },
+    },
 ];
 
 export default eslintConfig;
