826295f8155340798bfcf2162c27d0979c0c4a68
caio.vigiani <caio.vigiani@hotmail.com>
2026-05-31 11:51:49 -0300

    REVERT
    This reverts commit 1b98d29e75cec2add464e897b520da72092f98f1.
    A Estrutura "Inata" não ficou util

--- src/app/(paginas)/minhas-paginas/moderador/configuracao-seres-inatos/page.tsx
+++
@@ -1,3 +0,0 @@
-import PaginaModeradorConfiguracaoSeresInatos_Client from './componentes';
-
-export default function PaginaModeradorConfiguracaoSeresInatos() { return <PaginaModeradorConfiguracaoSeresInatos_Client />; };
